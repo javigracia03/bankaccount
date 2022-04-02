59b12cc5cf3ee3c365aa54ee884d9c8d79f89c03
javiergracia03 <javiergracia2003@gmail.com>
2022-04-02 10:26:54 +0200

    v2.0

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 
+
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
@@ -22,49 +23,73 @@ public class Main {
 
         Bankaccount a1 = new Bankaccount(username_enetered, password_enetered);
 
-        System.out.print("WHAT WOULD YOU LIKE TO DO?");
-        System.out.print("TYPE A FOR ADDIG MONEY TO YOUR ACCOUNT");
-        System.out.print("TYPE B FOR WITHDRAWING MONEY FROM YOUR ACCOUNT");
-        System.out.print("TYPE Q TO QUIT\n");
 
-        char option = scan.next().charAt(0);
-        char option1 = Character.toUpperCase(option);
+        boolean t = true;
+        boolean t2 = true;
+        while(t==true){
+            System.out.print("WHAT WOULD YOU LIKE TO DO?\n");
+            System.out.print("TYPE A FOR ADDIG MONEY TO YOUR ACCOUNT\n");
+            System.out.print("TYPE B FOR WITHDRAWING MONEY FROM YOUR ACCOUNT\n");
+            System.out.print("TYPE ANY OTHER LETTER TO QUIT\n");
 
+            char option = scan.next().charAt(0);
+            char option1 = Character.toUpperCase(option);
+    
 
-        switch (option1){
+            switch (option1){
 
-           case 'A': 
+            case 'A': 
 
-                System.out.println("HOW MUCH MONEY WOULD YOU LIKE TO ADD?");
+                    System.out.println("HOW MUCH MONEY WOULD YOU LIKE TO ADD?");
 
-                scan.nextLine();
+                    scan.nextLine();
 
-                String amount_entry = scan.nextLine();
+                    String amount_entry = scan.nextLine();
 
-                double amount = Double.parseDouble(amount_entry);
-                a1.entry(amount);
-                break;
-            
+                    double amount = Double.parseDouble(amount_entry);
+                    a1.entry(amount);
+                    break;
+                
             case 'B': 
 
-                System.out.println("HOW MUCH MONEY WOULD YOU LIKE TO SUBSTRACT?");
+                    System.out.println("HOW MUCH MONEY WOULD YOU LIKE TO SUBSTRACT?");
+                    
+                    scan.nextLine();
 
-                String amount_substracted = scan.nextLine();
+                    String amount_substracted = scan.nextLine();
 
-                double amount_sub = Double.parseDouble(amount_substracted);
-                a1.withdraw(amount_sub);
-                break;
-             
-            case 'Q': 
+                    double amount_sub = Double.parseDouble(amount_substracted);
+                    a1.withdraw(amount_sub);
+                    break;
+                
+            default : 
 
-                System.out.println("THANKS FOR TRUSTING US");
+                t2 = false;
+                t = false;
+                    break;
+                
+                
+            }//switch
 
-                break;
-            
+            if (t2==true){
+                System.out.println("YOUR CURRENT AMOUNT IS: " + a1.getmoney()+ "\n");
+                System.out.println("Would you like to do any other operation? Y/N");
+                char option2 = scan.next().charAt(0);
+                char option3 = Character.toUpperCase(option2);
 
-        }//switch
+                if (option3 == 'N'){
+                    t = false;
 
-        System.out.println("YOUR CURRENT AMOUNT IS: " + a1.getmoney());
+                } else scan.nextLine();
+            }//if    
 
-    }
+        }//while    
+
+       
+        System.out.println("THANKS FOR TRUSTING US");
+
+    }//main
+
+
+    
 }
